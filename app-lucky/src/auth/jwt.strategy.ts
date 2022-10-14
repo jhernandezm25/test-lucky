77f63260bfb3e390b8bfd3fc9f1574c84d4166a0
jhernandezm2 <jhernandezm@acid.cl>
2022-10-13 19:36:19 -0500

    :up: add environment var

--- app-lucky/src/auth/jwt.strategy.ts
+++ app-lucky/src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: 'abcdABCD1234554321'
+      secretOrKey: process.env.JWT_SECRET
     });
   }
 
